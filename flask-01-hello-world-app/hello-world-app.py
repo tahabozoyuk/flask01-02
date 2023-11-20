46659dc3093479595dfc17fef9d8222b67505d37
Taha <tahabozoyuk@gmail.com>
2023-11-20 22:35:37 +0300

    first commit

--- flask-01-hello-world-app/hello-world-app.py
+++ flask-01-hello-world-app/hello-world-app.py
@@ -2,7 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
+@app.route("/")
+def head():
+    return "hello world"
 
+@app.route("/second")
+def second():
+    return "This is second page"
+
+@app.route("/third")
+def third():
+    return 'This is third page'
 
 
 
